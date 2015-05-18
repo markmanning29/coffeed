ce3e4ea00e2dc1b71203b34c109615107a52b7bc
Mark Manning <mmanning@bundlbee.com>
2015-05-18 13:21:39 +0100

    3Rd Commit

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -1,7 +1,3 @@
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
-
 """
 WSGI config for coffeed project.
 
@@ -11,13 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+
 import os
-
-from django.core.wsgi import get_wsgi_application
-from djstatic import Cling
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
-
-application = Cling (get_wsgi_application())
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 
